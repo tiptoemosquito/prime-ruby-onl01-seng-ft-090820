8d9f50e4ff735b94c30cceb3a7b6200f296059e1
Christina Howard <clhoward33@icloud.com>
2020-09-13 20:15:38 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,9 @@
 def prime?(num)
   if num < 0 || num == 0 || num == 1
-    return false
+    return true
   else (2..num-1).to_a.all? do |factor|
   end
 end
 end
   # Add  code here!
+  num % factor != 0
